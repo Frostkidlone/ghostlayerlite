359d729b08ac6e7fb0a356aec6bc8f6a6650eb84
Abikure Patrick Akpovona <freshboykinky@gmail.com>
2026-04-29 17:46:54 -0700

    Add files via upload

---
+++ api.js
@@ -0,0 +1,15 @@
+{
+  "name": "ghostlayer",
+  "version": "1.0.0",
+  "dependencies": {
+    "next": "latest",
+    "react": "latest",
+    "react-dom": "latest",
+    "@polkadot/api": "latest",
+    "@polkadot/api-contract": "latest",
+    "@polkadot/extension-dapp": "latest"
+  },
+  "scripts": {
+    "dev": "next dev"
+  }
+}
